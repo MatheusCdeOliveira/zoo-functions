c597296e55ecda1edc558d776051857fb6019b5b
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-28 18:29:50 -0300

    requisito 3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const funcionario = data.employees.find(
+    (element) =>
+      element.firstName === employeeName || element.lastName === employeeName,
+  );
+  if (!employeeName) {
+    return {};
+  }
+  return funcionario;
 }
 
 module.exports = getEmployeeByName;
