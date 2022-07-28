c83c7289719833bc4e998ae2c946edba46f20554
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-28 17:00:38 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu  código aqui
+function getSpeciesByIds(...ids) {
+  const especie = data.species.filter((element) => ids.includes(element.id));
+  return especie;
 }
 
 module.exports = getSpeciesByIds;
