3161222dce11f6c9d0cb48daacb15b0f7b6cf887
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-28 17:45:16 -0300

    requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const bicho = data.species.find((element) => element.name === animal);
+  const idade = bicho.residents;
+  return idade.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
