dd6ad372010018d47b32aeff53b0587b196ffe44
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-29 13:35:23 -0300

    requisito 12, 13 e 16 feitos

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,29 +1,31 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const pessoa = data.employees.find((element) => element.managers);
+  const pessoa = data.employees.find((elemento) => elemento.managers);
   return pessoa.managers.some((gerente) => gerente === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // let resultado = []
-  // if(isManager(managerId) === true){
-  //   employees.forEach((element) => {
-  //     if(element.id === managerId){
-  //     resultado.push(`${element.firstName} ${element.lastName}`)
-  //     }
-  //   })
-  // }
-
-  // return resultado
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } const gerente = employees.filter((element) => element.managers.includes(managerId))
+    .map((valor) => `${valor.firstName} ${valor.lastName}`);
+  return gerente;
 }
-// if(isManager(managerId)){
-//  const gerente = employees.filter((element) => {
-//   return element.id === managerId
-//  })
-//  return gerente.
-// }
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// let resultado = [];
+// if (isManager(managerId)) {
+//   employees.forEach((element) => {
+//     if (element.managers.includes(managerId)) {
+//       resultado.push(`${element.firstName} ${element.lastName}`);
+//     }
+//   });
+// }
+// return resultado;
+
+// let array = ["Burl Bethea", "Ola Orloff", "Emery Elser"];
+
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,53 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+const expected = {
+  Tuesday: { open: 8, close: 6 },
+  Wednesday: { open: 8, close: 6 },
+  Thursday: { open: 10, close: 8 },
+  Friday: { open: 10, close: 8 },
+  Saturday: { open: 8, close: 10 },
+  Sunday: { open: 8, close: 8 },
+  Monday: { open: 0, close: 0 },
+};
+
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se não passando argumentos, a função retorna o objeto', () => {
+    expect(getOpeningHours()).toEqual(expected);
+  });
+
+  it('Verifica se ao passar os argumentos Monday e 09:00-AM retorna a string: The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toMatch('The zoo is closed');
+  });
+
+  it('Verifica se ao passar os argumentos Tuesday e 09:00-AM retorna a string: The zoo is open', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toMatch('The zoo is open');
+  });
+
+  it('Verifica se ao passar os argumentos Wednesday e 09:00-PM retorna a string: The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toMatch('The zoo is closed');
+  });
+
+  it('Verifica se ao passar os argumentos Thu e 09:00-PM retorna o erro: The day must be valid. Example: Monday', () => {
+    expect(() => getOpeningHours('Thu', '09:00-PM')).toThrow(Error);
+  });
+
+  it('Verifica se ao passar os argumentos Friday e 09:00-ZM retorna o erro: The abbreviation must be AM or PM', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow(Error);
+  });
+
+  it('Verifica se ao passar os argumentos Saturday e C9:00-AM retorna o erro: The hour should represent a number', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(Error);
+  });
+
+  it('Verifica se ao passar os argumentos Sunday e 09:c0-AM retorna o erro: The minutes should represent a number', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(Error);
+  });
+
+  it('Verifica se ao passar os argumentos Monday e 13:00-AM retorna o erro: The hour must be between 0 and 12', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow(Error);
+  });
+
+  it('Verifica se ao passar os argumentos Tuesday e 09:60-AM retorna o erro: The minutes must be between 0 and 59', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow(Error);
+  });
+});
