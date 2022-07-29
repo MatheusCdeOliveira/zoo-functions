09bae2714dd0742a15056ba554d7a0890a2103d3
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-29 19:59:09 -0300

    requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,24 +4,27 @@ const data = require('../data/zoo_data');
 function countAnimals(animal) {
   if (!animal) {
     return species.reduce((acc, curr) => {
-      if (!Object.keys(acc).includes(curr.name)) {
-        acc[curr.name] = curr.residents.length;
-      }
+      acc[curr.name] = curr.residents.length;
       return acc;
     }, {});
   }
-  if (animal.specie) {
-    const animalCapturado = data.species.find((element) => element.name === animal.specie);
-    let retorno = animalCapturado.residents.length;
-    if (animal.sex) {
-      retorno = animalCapturado.residents.reduce((acc, curr) => {
-        (curr.sex === animal.sex) ? acc += 1 : acc;
-        return acc;
-      }, 0);
-    }
-    return retorno;
+  const animalCapturado = data.species.find((element) => element.name === animal.specie);
+  let retorno = animalCapturado.residents.length;
+  if (animal.sex) {
+    retorno = animalCapturado.residents.filter((element) => element.sex === animal.sex);
+    retorno = retorno.length;
   }
+  return retorno;
 }
 console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+
+// if (animal.sex) {
+//   retorno = animalCapturado.residents.reduce((acc, curr) => {
+//     if (curr.sex === animal.sex) {
+//       acc += 1;
+//     }
+//     return acc;
+//   }, 0);
+// }
 
 module.exports = countAnimals;
