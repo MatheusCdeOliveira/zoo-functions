de9381ef2a9f65a59f4b64bc7094a8a854ae24e8
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-30 18:58:29 -0300

    requisito 8 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const resultado = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      resultado.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      resultado.adult += 1;
+    } else if (element.age >= 50) {
+      resultado.senior += 1;
+    }
+  });
+  return resultado;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) {
+    return 0;
+  }
+  const pessoas = countEntrants(entrants);
+  const resultChild = pessoas.child * prices.child;
+  const resultAdult = pessoas.adult * prices.adult;
+  const resultSenior = pessoas.senior * prices.senior;
+  return resultChild + resultAdult + resultSenior;
 }
-
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
