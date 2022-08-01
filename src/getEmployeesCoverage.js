1e7168fd638edad219c3d34287729fa3ad40a3d7
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-08-01 20:37:40 -0300

    requisito 11 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const expectedObject = (specie) => {
+  const getEmployee = data.employees.find((person) => person.firstName === specie.name
+  || person.lastName === specie.name || person.id === specie.id);
+  if (getEmployee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const getSpecie = getEmployee.responsibleFor.map((element) => {
+    const animais = species.filter((a) => element === a.id);
+    return animais[0].name;
+  });
+  const loc = getEmployee.responsibleFor.map((element) => {
+    const animais = species.filter((b) => element === b.id);
+    return animais[0].location;
+  });
+  const fullName = `${getEmployee.firstName} ${getEmployee.lastName}`;
+  const obj = { id: getEmployee.id, fullName: `${fullName}`, species: getSpecie, locations: loc };
+  return obj;
+};
+
+const getPerson = (id) => species.find((specie) => specie.id === id);
+
+function getEmployeesCoverage(specie) {
+  if (!specie) {
+    return employees.map((employe) => ({
+      id: employe.id,
+      fullName: `${employe.firstName} ${employe.lastName}`,
+      species: employe.responsibleFor.map((a) => getPerson(a).name),
+      locations: employe.responsibleFor.map((a) => getPerson(a).location),
+    }));
+  }
+  return expectedObject(specie);
 }
 
 module.exports = getEmployeesCoverage;
